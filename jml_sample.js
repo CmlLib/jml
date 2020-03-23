f6211473dd9a74a29b7667239a741aa15590df5a
AlphaBs <ksi123456ab@naver.com>
2020-03-23 20:56:30 +0900

    add forge installer

--- jml_sample.js
+++ jml_sample.js
@@ -1,20 +1,33 @@
-const jml = require("./lib/jml");
-//const jml = require("minecraft-jml");
+const jml = require("./lib/jml.js");
 
 async function init() {
     var p = "./game_dir"
-    await jml.initialize(p);
 
-    var profiles = await jml.updateProfiles();
-    console.log("profiles : ");
-    for (var i = 0; i < profiles.length; i++) {
-        console.log(profiles[i].name);
+    var launcher = new jml.jml();
+    await launcher.initialize(p);
+
+    console.log("initialized in " + launcher.getGamePath());
+
+    launcher.downloadEventHandler = function (kind, name, max, current) {
+        console.log(`${kind} - ${name} (${current} / ${max})`);
+    };
+
+    var mcversion = "1.12.2";
+    var forgeversion = "14.23.5.2847";
+    var versionname = launcher.getVersionName(mcversion, forgeversion);
+
+    await launcher.updateProfiles();
+
+    if (!launcher.profiles.some(x => x.name === versionname)) { // forge not installed
+        console.log("install forge : " + versionname);
+
+        await launcher.downloadForge(mcversion, forgeversion);
+        await launcher.updateProfiles();
     }
 
-    var version = "1.5.2";
-    //var version = "1.12.2";
+    console.log("start " + versionname);
 
-    var arg = await jml.launch(version, {
+    var arg = await launcher.launch(versionname, {
         xmx: 4096,
         server_ip: "mc.hypixel.net",
         session: {
@@ -24,8 +37,6 @@ async function init() {
         },
         screen_width: 1600,
         screen_height: 900
-    }, function (kind, name, max, current) {
-        console.log(`${kind} - ${name} (${current} / ${max})`);
     });
 
     console.log(arg);
